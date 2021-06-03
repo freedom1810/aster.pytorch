6aa46d39fdc73fbf14e2f6e741e8985349ce3937
freedom1810 <nguyenhaison181097@gmail.com>
2021-06-03 12:30:32 +0900

    change logging merge with tensorboard v2.x

--- lib/utils/logging.py
+++ lib/utils/logging.py
@@ -65,13 +65,17 @@ class TFLogger(object):
     """Create a summary writer logging to log_dir."""
     if log_dir is not None:
       mkdir_if_missing(log_dir)
-    self.writer = tf.summary.FileWriter(log_dir)
+    # self.writer = tf.summary.FileWriter(log_dir)
+      self.writer = tf.summary.create_file_writer(log_dir)
 
   def scalar_summary(self, tag, value, step):
     """Log a scalar variable."""
-    summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-    self.writer.add_summary(summary, step)
-    self.writer.flush()
+    # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+    # self.writer.add_summary(summary, step)
+    # self.writer.flush()
+    with self.writer.as_default():
+      tf.summary.scalar(tag, value, step=step)
+      self.writer.flush()
 
   def image_summary(self, tag, images, step):
     """Log a list of images."""
